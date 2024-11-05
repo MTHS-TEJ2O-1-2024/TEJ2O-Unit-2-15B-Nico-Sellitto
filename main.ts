e86aa4c3e2a3f3be2240d40f2ae12b4f0622923b
Nico-Sellitto <nico.sellitto@mths.ca>
2024-11-05 10:49:02 -0500

    imported code

--- main.ts
+++ main.ts
@@ -1,8 +1,89 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nico Sellitto
+ * Created on: Nov 2024
+ * This program moves a pixel around a screen
 */
 
-basic.showString('Hello, World!')
+let sprite: game.LedSprite = null
+let loopCount = 0
+
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// sends the pixel around the screen clockwise
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCount = 0
+    sprite = game.createSprite(0, 0)
+
+    //right
+    while (loopCount <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCount)
+        loopCount = loopCount + 1
+        basic.pause(500)
+    }
+
+    // down
+    loopCount = 0
+    while (loopCount <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCount)
+        loopCount = loopCount + 1
+        basic.pause(500)
+    }
+    //left
+    loopCount = 5
+    while (loopCount >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCount)
+        loopCount = loopCount - 1
+        basic.pause(500)
+    }
+    //up
+    loopCount = 5
+    while (loopCount >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCount)
+        loopCount = loopCount - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// sends the pixel around the screen counter clockwise
+input.onButtonPressed(Button.B, function () {
+    // setup
+    basic.clearScreen()
+    loopCount = 0
+    sprite = game.createSprite(0, 0)
+    //down
+    while (loopCount <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCount)
+        loopCount = loopCount + 1
+        basic.pause(500)
+    }
+    //Right
+    loopCount = 0
+    while (loopCount <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCount)
+        loopCount = loopCount + 1
+        basic.pause(500)
+    }
+    //up
+    loopCount = 5
+    while (loopCount >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCount)
+        loopCount = loopCount - 1
+        basic.pause(500)
+    }
+    //left
+    loopCount = 5
+    while (loopCount >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCount)
+        loopCount = loopCount - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
