8634746453b5f012dcbab68f9da1ff5cbf90a1b8
Nico-Sellitto <nico.sellitto@mths.ca>
2024-11-05 11:20:13 -0500

    coe

--- main.ts
+++ main.ts
@@ -13,9 +13,21 @@ basic.showIcon(IconNames.Happy)
 
 // sends the pixel around the screen clockwise
 input.onButtonPressed(Button.A, function () {
-
-
-    sprite.delete()
-    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+    loopCount = 0
+    sprite = game.createSprite(0, 0)
+    while (loopCount <= 5) {
+    basic.pause(500)
+    loopCount + 1
+    sprite.move(1)
+        while (loopCount >= 5) {
+            sprite.turn(Direction.Right, 90)
+            loopCount = 0
+            basic.pause(500)
+        }
+    }
 })
 
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(loopCount)
+})
